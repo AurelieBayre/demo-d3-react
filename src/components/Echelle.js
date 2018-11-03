e7a09f4c7ce8e5310c26587de4d3b89f7c875b50
Aurelie Bayre <a.bayre@gmail.com>
2018-11-03 22:38:54 +0100

    feat: add addData to test scales

--- src/components/Echelle.js
+++ src/components/Echelle.js
@@ -1,42 +1,81 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {scaleBand, scaleLinear, max} from 'd3';
 
-const data = [12, 5, 6, 6, 9, 10, 8, 5, 6, 6, 9, 10, 8];
-
-const width = 700;
-const height = 300;
-
 // Attention voici les echelles ! On les définit pour chaque axe: x et y
-const x = scaleBand() // définition du type d'échelle
-  .rangeRound([0, width]) //définition des limites de l'échelle
-  .padding(0.1);
-
-const y = scaleLinear().rangeRound([height, 0]);
+// définition du type d'échelle
+//définition des limites de l'échelle
 
 // Et il faut relier les axes aux données:
-x.domain(data.map((d, i) => i));
-y.domain([0, max(data, d => d)]);
 
-export default () => (
-  <div>
-    <h1>Les échelles</h1>
-    <svg width={width} height={height}>
-      {data.map((d, i) => {
-        return (
-          <rect
-            key={i}
-            className="bar"
-            x={x(i)}
-            y={y(d)}
-            width={x.bandwidth()}
-            /*
-            littéralement largeur de bande aka largeur de la barre. 
-            Elle est calculée en fonction du nombre d'items dans le dataset.
-          */
-            height={height - y(d)}
-          />
-        );
-      })}
-    </svg>
-  </div>
-);
+export default class Scale extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [
+        {value: 12, color: '#40DA00'},
+        {value: 5, color: '#C08503'},
+        {value: 6, color: '#006069'},
+        {value: 6, color: '#012450'},
+        {value: 9, color: '#123456'},
+        {value: 10, color: '#654321'},
+        {value: 8, color: '#AAFF00'}
+      ],
+      width: 600,
+      height: 300
+    };
+  }
+  addData(e) {
+    e.preventDefault();
+    const randomValue = Math.random() * 20;
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    const newDataPoint = {
+      value: randomValue,
+      color: '#' + randomColor
+    };
+    this.setState({data: [...this.state.data, newDataPoint]});
+  }
+
+  render() {
+    const data = this.state.data;
+    const width = this.state.width;
+    const height = this.state.height;
+
+    const x = scaleBand()
+      .rangeRound([0, width])
+      .padding(0.1);
+
+    const y = scaleLinear().rangeRound([height, 0]);
+
+    x.domain(data.map((d, i) => i));
+    y.domain([0, max(data, d => d.value)]);
+    console.log('data in Echelle', data);
+
+    return (
+      <div>
+        <h1>Les échelles</h1>
+        <form onSubmit={e => this.addData(e)}>
+          <div>
+            <label>Ajouter une valeur</label>
+          </div>
+          <button type="submit">Ajout</button>
+        </form>
+        <svg width={width} height={height}>
+          {data.map((d, i) => {
+            return (
+              <rect
+                key={i}
+                className="bar"
+                x={x(i)}
+                y={y(d.value)}
+                height={height - y(d.value)}
+                width={x.bandwidth()}
+                fill={d.color}
+              />
+            );
+          })}
+        </svg>
+      </div>
+    );
+  }
+}
