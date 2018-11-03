7894352fd309e1bddec5b4eeb5028ad83efcbc54
Aurelie Bayre <a.bayre@gmail.com>
2018-11-03 09:31:16 +0100

    feat: with random colors

--- src/components/Svg.js
+++ src/components/Svg.js
@@ -1,21 +1,47 @@
 import React, {Component} from 'react';
-//import * as d3 from "d3";
+// Exemple juste avec react, sans D3:
 
 export default class Svg extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {value: 12, color: '#000000'},
+        {value: 5, color: '#000000'},
+        {value: 6, color: '#000000'},
+        {value: 6, color: '#000000'},
+        {value: 9, color: '#000000'},
+        {value: 10, color: '#000000'},
+        {value: 8, color: '#000000'}
+      ]
+    };
+  }
+  getColor(arr) {
+    const dataWithRandomColors = arr.map(obj => {
+     const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+     return {value: obj.value,
+            color: "#"+randomColor
+      }
+    })
+    return dataWithRandomColors;
+  };
+  
+
   render() {
-    const data = [12, 5, 6, 6, 9, 10, 8];
+    const newData = this.getColor(this.state.data);
     return (
       <div>
         <h1>Devinette : pourquoi obtient-on cela? </h1>
         <svg width="700" height="300">
-          {data.map((d, i) => (
+          {newData.map((d, i) => (
             <rect
               key={i}
               className="bar"
               x={i * 70}
-              y={d}
+              y={d.value}
               width={25}
-              height={d * 10}
+              height={d.value * 10}
+              fill={d.color}
             />
           ))}
         </svg>
--- src/components/Svg2.js
+++ src/components/Svg2.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-//import * as d3 from "d3";
 
 export default class Svg2 extends Component {
   render() {
