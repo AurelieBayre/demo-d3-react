6fe1b5941224a4def06d962527c6b7530ae8ea5d
Aurelie Bayre <a.bayre@gmail.com>
2018-11-13 08:18:10 +0100

    refactor: svg2 => same rhythm as svg

--- src/components/Svg2.js
+++ src/components/Svg2.js
@@ -8,7 +8,7 @@ export default class Svg2 extends Svg {
     this.interval = setInterval(() => {
       const newArray = this.changeData();
       this.setState({data: newArray});
-    }, 3000);
+    }, 1000);
   }
 
   componentWillUnmount() {
